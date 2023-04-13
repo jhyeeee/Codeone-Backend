edd449e4c37dee2f5578b54e2a3fa78425805ea9
yichangshin <ycs318@naver.com>
2023-04-14 02:14:20 +0900

    #5 스터디 모집 글 삭제
    - 서비스 추가

--- src/main/java/com/codeone/service/studygroup/StudygroupService.java
+++ src/main/java/com/codeone/service/studygroup/StudygroupService.java
@@ -1,5 +1,6 @@
 package com.codeone.service.studygroup;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codeone.command.studygroup.StudygroupCommand;
 import com.codeone.dao.studygroup.StudygroupDao;
 import com.codeone.dao.studygroup.StudygroupPositionDao;
 import com.codeone.dao.studygroup.StudygroupStackDao;
 import com.codeone.dto.studygroup.StudygroupDto;
 import com.codeone.dto.studygroup.StudygroupPositionDto;
 import com.codeone.dto.studygroup.StudygroupStackDto;
+import com.codeone.exception.AlreadyStudygroupStartedException;
 
 @Service
 @Transactional
@@ -24,9 +27,35 @@ public class StudygroupService {
 	@Autowired
 	StudygroupStackDao studygroupStackDao;
 	
-	public void writeStudyGroupRecruitment(StudygroupDto studygroup) {
+	public boolean deleteStudygroupRecruitment(StudygroupCommand studygroup) throws AlreadyStudygroupStartedException {
+		// 삭제를 요청한 사용자의 번호
+		int requestMemberSeq = studygroup.getMemberSeq();
+		// 삭제를 요청한 날짜
+		LocalDate now = LocalDate.now();
+		
+		// 삭제할 모집글 조회
+		StudygroupDto oldStudygroup = studygroupDao.selectOneBySeq(studygroup);
+		if(oldStudygroup == null || oldStudygroup.getMemberSeq() != requestMemberSeq) {
+			// 존재 하지 않는 seq 번호를 전달했거나 삭제할 모집글의 작성자가 아닐 경우
+			return false;
+		} else if(oldStudygroup.isDeleted()) {
+			// 이미 삭제 처리된 모집 글에 다시 삭제 요청을 했을 경우
+			return true;
+		} else if(oldStudygroup.getStartDate().isBefore(now)) {
+			// 이미 시작된 스터디그룹일 경우
+			throw new AlreadyStudygroupStartedException();
+		}
+		
+		// 삭제할 모집글의 작성자일 경우
+		// 모집글 삭제 처리
+		studygroupDao.deleteStudygroupRecruitment(studygroup.getSeq());
+		
+		return true;
+	}
+	
+	public void writeStudygroupRecruitment(StudygroupDto studygroup) {
 		// 모집글 작성
-		studygroupDao.writeStudyGroupRecruitment(studygroup);
+		studygroupDao.writeStudygroupRecruitment(studygroup);
 		
 		// 모집글 번호
 		int seq = studygroup.getSeq();
@@ -60,9 +89,5 @@ public class StudygroupService {
 		
 		studygroupStackDao.insert(StudygroupStackList);
 		// -- 모집글의 기술 스택 등록 --
-	}
-	
-	public List<StudygroupDto> getAll() {
-		return studygroupDao.getAll();
 	}
 }
