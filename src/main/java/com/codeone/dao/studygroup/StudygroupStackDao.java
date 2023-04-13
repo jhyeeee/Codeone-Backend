3e9e790c50db0dc97fbd65e7c7ae38d22010efbd
yichangshin <ycs318@naver.com>
2023-04-14 01:21:30 +0900

    #3 스터디 모집 글 작성
    - DAO와 sql 추가

---
+++ src/main/java/com/codeone/dao/studygroup/StudygroupStackDao.java
@@ -0,0 +1,14 @@
+package com.codeone.dao.studygroup;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.codeone.dto.studygroup.StudygroupStackDto;
+
+@Mapper
+@Repository
+public interface StudygroupStackDao {
+	void insert(List<StudygroupStackDto> studygroupStackDtoList);
+}
