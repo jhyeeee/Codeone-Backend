f93482f268205501ba5772d4895b9762b001dde9
songyi219 <mi538@naver.com>
2023-04-14 18:49:12 +0900

    #4 test

--- src/main/java/com/codeone/controller/store/storeController.java
+++ src/main/java/com/codeone/controller/store/storeController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.codeone.dto.store.storeItemDto;
 import com.codeone.service.store.storeService;
 
@@ -17,8 +16,8 @@ public class storeController {
 	
 	// 중고거래 글쓰기
 	@PostMapping(value = "/storewrite")
-	public String storewrite(storeItemDto item) {
-		System.out.println("storeController storewrite() " + new Date());
+	public String storewrite(storeItemDto item) {		
+		System.out.println("storeController storewrite() " + new Date());			
 		
 		boolean isWriteStore = service.writeStore(item);
 		if(isWriteStore == true) {
