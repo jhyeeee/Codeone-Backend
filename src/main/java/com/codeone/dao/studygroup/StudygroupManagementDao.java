0ffd1b73b59e6cc060e0f0d71d5f510cfc6d54e3
yichangshin <ycs318@naver.com>
2023-04-14 16:00:16 +0900

    #3 스터디 모집 글 작성
    - 스터디 그룹 관리 DTO, DAO 추가

---
+++ src/main/java/com/codeone/dao/studygroup/StudygroupManagementDao.java
@@ -0,0 +1,13 @@
+package com.codeone.dao.studygroup;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.codeone.dto.studygroup.StudygroupManagementDto;
+
+@Mapper
+@Repository	
+public interface StudygroupManagementDao {
+	void insert(StudygroupManagementDto newStudygroupManagement);
+	void updateInfoSeq(StudygroupManagementDto newStudygroupManagement);
+}
