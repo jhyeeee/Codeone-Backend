0bde7f284c55084d5249f1a35263f238c8e94bbf
yichangshin <ycs318@naver.com>
2023-04-16 23:35:05 +0900

    #1 스터디 그룹 글 목록
    - 스터디 그룹 글 상세 컨트롤러 병합
    - 스터디 그룹 글 목록 컨트롤러 추가
    - 스터디 그룹 글 목록 컨트롤러에서 사용할 커맨드 객체 추가
    - 스터디 그룹 글 목록 컨트롤러에서 사용할 커맨드 객체 Validator 추가
    - 스터디 그룹 글 목록 컨트롤러에서 사용할 스크롤 당 보여줄 컨텐츠의 수 상수 추가

--- src/main/java/com/codeone/controller/studygroup/StudyGroupMainController.java
+++ src/main/java/com/codeone/controller/studygroup/StudyGroupMainController.java
@@ -1,30 +1,46 @@
 package com.codeone.controller.studygroup;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codeone.command.studygroup.StudygroupDeleteCommand;
 import com.codeone.command.studygroup.StudygroupInfoCommand;
+import com.codeone.command.studygroup.StudygroupListCommand;
 import com.codeone.command.studygroup.StudygroupUpdateCommand;
+import com.codeone.dto.studygroup.StudygroupDetailDto;
+import com.codeone.dto.studygroup.StudygroupListDto;
+import com.codeone.etc.StaticVariable;
+import com.codeone.exception.DeletedStudygroupException;
 import com.codeone.exception.NotPermissionToModifyException;
+import com.codeone.exception.StudygroupNotFoundException;
+import com.codeone.service.studygroup.StudygroupDetailService;
 import com.codeone.service.studygroup.StudygroupInfoService;
 import com.codeone.validator.studygroup.DeleteStudygroupValidator;
+import com.codeone.validator.studygroup.StudygroupListValidator;
 import com.codeone.validator.studygroup.UpdateStudygroupValidator;
 import com.codeone.validator.studygroup.WriteStudygroupValidator;
 
 @RestController
+@RequestMapping("/studygroup")
 public class StudyGroupMainController {
 	@Autowired
-	StudygroupInfoService studygroupService;
+	StudygroupInfoService studygroupInfoService;
+	@Autowired
+	StudygroupDetailService studygroupDetailService;
 	
-	@PostMapping("/studygroup")
+	@PostMapping()
 	public ResponseEntity<Void> writeStudygroupRecruitment(StudygroupInfoCommand studygroupCommand, BindingResult errors) {
 		// 로그인 여부 확인 코드 필요
 		int memberSeq = 1;
@@ -42,12 +58,12 @@ public class StudyGroupMainController {
 		studygroupCommand.setMemberSeq(memberSeq);
 		
 		// 모집글 작성
-		studygroupService.writeStudygroupRecruitment(studygroupCommand);
+		studygroupInfoService.writeStudygroupRecruitment(studygroupCommand);
 		
 		return ResponseEntity.ok().build();
 	}
 	
-	@DeleteMapping("/studygroup")
+	@DeleteMapping()
 	public ResponseEntity<Void> deleteStudygroupRecruitment(StudygroupDeleteCommand studygroupDeleteCommand, BindingResult errors) {
 		// 로그인 여부 확인 코드 필요
 		int memberSeq = 1;
@@ -67,7 +83,7 @@ public class StudyGroupMainController {
 		
 		try {
 			// 모집글 삭제
-			boolean result = studygroupService.deleteStudygroupRecruitment(studygroup);
+			boolean result = studygroupInfoService.deleteStudygroupRecruitment(studygroup);
 			
 			if(result) {
 				// 모집글을 삭제했을 경우
@@ -82,7 +98,7 @@ public class StudyGroupMainController {
 		}
 	}
 	
-	@PutMapping("/studygroup")
+	@PutMapping()
 	public ResponseEntity<Void> updateStudygroupRecruitment(StudygroupUpdateCommand studygroupUpdateCommand, BindingResult errors) {
 		// 로그인 여부 확인 코드 필요
 		int memberSeq = 1;
@@ -102,7 +118,7 @@ public class StudyGroupMainController {
 		
 		try {
 			// 모집글 수정
-			boolean result = studygroupService.updateStudygroupRecruitment(studygroupUpdateCommand);
+			boolean result = studygroupInfoService.updateStudygroupRecruitment(studygroupUpdateCommand);
 			
 			if(result) {
 				// 모집글 수정 완료
@@ -116,4 +132,48 @@ public class StudyGroupMainController {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 		}
 	}
+	
+	@GetMapping("/detail/{seq}")
+	public ResponseEntity<StudygroupDetailDto> getStudygroupInfo(@PathVariable("seq") int seq) {
+		// seq 검증
+		if(seq <= 0) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		try {
+			// 스터디 그룹 정보 조회
+			StudygroupDetailDto studygroupDetail = studygroupDetailService.getStudygroupInfo(seq);
+			
+			return ResponseEntity.ok(studygroupDetail);
+		} catch(StudygroupNotFoundException |  DeletedStudygroupException e) {
+			// StudygroupNotFoundException -> 잘못된 seq를 보내 스터디 그룹을 찾지 못했거나
+			// DeletedStudygroupException -> 삭제된 스터디 그룹의 상세 정보를 요청했을 경우
+			return ResponseEntity.noContent().build();
+		}
+	}
+	
+	@GetMapping("/list")
+	public ResponseEntity<List<StudygroupListDto>> getStudygroupList(StudygroupListCommand studygroupListCommand, BindingResult errors) {
+		Validator validator = new StudygroupListValidator();
+		// 필터링 값을 전달 받았을 경우 전달 받은 필터링 값 검증
+		validator.validate(studygroupListCommand, errors);
+		
+		if(errors.hasErrors()) {
+			// 클라이언트가 잘못된 값을 보냈다면
+			return ResponseEntity.badRequest().build();
+		}
+		
+		// 페이지 번호에 맞게 검색 범위 설정
+		studygroupListCommand.setStart(studygroupListCommand.getDepth() * StaticVariable.DEPTH_PER_CONTENTS_AMOUNT);
+		studygroupListCommand.setEnd((studygroupListCommand.getDepth() + 1) * StaticVariable.DEPTH_PER_CONTENTS_AMOUNT);
+		
+		List<StudygroupListDto> studygroupList = studygroupInfoService.getStudygroupList(studygroupListCommand);
+		
+		if(studygroupList.size() == 0 || studygroupList.get(0) == null) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.ok(studygroupList);
+		}
+	}
+	
 }
--- src/main/java/com/codeone/etc/StaticVariable.java
+++ src/main/java/com/codeone/etc/StaticVariable.java
@@ -1,6 +1,9 @@
 package com.codeone.etc;
 
 public class StaticVariable {
+	// 공통
+	public final static int DEPTH_PER_CONTENTS_AMOUNT = 25;	// 스크롤 당 보여줄 컨텐츠의 수
+	
 	// 스터디그룹
 	public final static int POSITION_MIN = 1;	// 모집 분야 최소값
 	public final static int POSITION_MAX = 7;	// 모집 분야 최대값
@@ -9,10 +12,5 @@ public class StaticVariable {
 	public final static int TECHNOLOGY_STACK_MAX = 20;	// 기술 스택 최대값
 	// 스터디그룹
 
-	// 스터디그룹 투표 유형
 	
-	// 스터디그룹 투표 유형
-	public final static int VOTING1 = 1;	// 스터디그룹 삭제
-	public final static int VOTING2 = 2;	// 스터디그룹 수정
-	// 스터디그룹 투표 유형
 }
