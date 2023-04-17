bb0ace96af510069295da2a6df6be58cd4654cd1
jhyeeee <104542485+jhyeeee@users.noreply.github.com>
2023-04-17 10:13:37 +0900

    #16 일정관리 일정추가 db에 일정추가 되는거까지 확인

---
+++ src/main/java/com/codeone/controller/calendar/CalendarController.java
@@ -0,0 +1,49 @@
+package com.codeone.controller.calendar;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.codeone.dto.calendar.CalendarDto;
+import com.codeone.service.calendar.CalendarService;
+
+
+@RestController
+public class CalendarController {
+
+	@Autowired
+	CalendarService service;
+	
+	@GetMapping(value = "/getCalendarList")
+	public List<CalendarDto> getCalendarList(){
+		System.out.println("CalendarController getCalendarList() " + new Date());
+		String id = "aaa";
+		
+		List<CalendarDto> list = service.getCalendarList(id);
+		System.out.println("////////확인 " + list);
+		return list;
+	}
+	
+	
+//	일정추가
+	@PostMapping(value="/writeCalendar")
+    public String writeCalendar(CalendarDto dto) {
+		System.out.println("CalendarController writeCalendar() " + new Date());
+		System.out.println("/////////////" + dto);
+		boolean result = service.writeCalendar(dto);
+        if(result == true) {
+        	return "YES";        
+        }
+        return "NO";
+        
+    }
+}
---
+++ src/main/java/com/codeone/dto/calendar/CalendarDto.java
@@ -0,0 +1,100 @@
+package com.codeone.dto.calendar;
+
+import java.io.Serializable;
+
+public class CalendarDto implements Serializable{
+	
+	private int seq;
+	private String id;
+	private String title;
+	private String content;
+	private String start;
+	private String end;
+	private String backgroundColor;
+	private String textColor;
+	private boolean allDay;
+	
+	public CalendarDto() {
+		
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	public void setBackgroundColor(String backgroundColor) {
+		this.backgroundColor = backgroundColor;
+	}
+
+	public String getTextColor() {
+		return textColor;
+	}
+
+	public void setTextColor(String textColor) {
+		this.textColor = textColor;
+	}
+
+	public boolean isAllDay() {
+		return allDay;
+	}
+
+	public void setAllDay(boolean allDay) {
+		this.allDay = allDay;
+	}
+
+	@Override
+	public String toString() {
+		return "CalendarDto [seq=" + seq + ", id=" + id + ", title=" + title + ", content=" + content + ", start="
+				+ start + ", end=" + end + ", backgroundColor=" + backgroundColor + ", textColor=" + textColor
+				+ ", allDay=" + allDay + "]";
+	}
+	
+}
