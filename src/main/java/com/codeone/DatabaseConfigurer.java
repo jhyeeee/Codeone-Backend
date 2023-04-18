84355a9b688f01fdc3f075646b81cbc0f1606ece
kwatermelon <hanamango@naver.com>
2023-04-18 10:01:25 +0900

    test2

--- src/main/java/com/codeone/DatabaseConfigurer.java
+++ src/main/java/com/codeone/DatabaseConfigurer.java
@@ -24,34 +24,28 @@ public class DatabaseConfigurer {
 	@ConfigurationProperties(prefix="spring.datasource.hikari")
 	public HikariConfig hikariConfig() {
 		return new HikariConfig();
-	}
-	
+	}	
 	@Bean
 	public DataSource dataSource() {
 		DataSource dataSource = new HikariDataSource(hikariConfig());
 		System.out.println("dataSource" + dataSource);
 		return dataSource;
+	}	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		System.out.println("DatabaseConfig sqlSessionFactory");
+		
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		
+		Resource[] arrResource = new PathMatchingResourcePatternResolver().getResources("classpath:sqls/*.xml");
+		sqlSessionFactoryBean.setMapperLocations(arrResource);
+		sqlSessionFactoryBean.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
+		
+		return (SqlSessionFactory)sqlSessionFactoryBean.getObject();				
+	}	
+	@Bean
+	public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
 	}
-	
-//	@Bean
-//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-//		System.out.println("DatabaseConfig sqlSessionFactory");
-//		
-//		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-//		sqlSessionFactoryBean.setDataSource(dataSource);
-//		
-//		Resource[] arrResource = new PathMatchingResourcePatternResolver().getResources("classpath:sqls/*.xml");
-//		sqlSessionFactoryBean.setMapperLocations(arrResource);
-//		sqlSessionFactoryBean.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
-//		
-//		return (SqlSessionFactory)sqlSessionFactoryBean.getObject();		
-//		
-//	}
-//	
-//	@Bean
-//	public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
-//		return new SqlSessionTemplate(sqlSessionFactory);
-//	}
-	
-	
 }
